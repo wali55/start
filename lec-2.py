d089a779a5f7234ca35bddd23174b8c498f16ee5
wali55 <walisantunu@gmail.com>
2023-11-28 14:28:38 +0600

    python number

---
+++ lec-2.py
@@ -0,0 +1,23 @@
+"""
+x = 35e3
+y = 12E4
+z = -87.7e100
+a = 3e8
+
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+print(a, type(a))
+
+import random
+
+print(random.randrange(1, 6))
+"""
+x = int(1)
+y = int(2.8)
+z = int("3")
+a = int("5")
+print(x)
+print(y)
+print(z)
+print(a)
